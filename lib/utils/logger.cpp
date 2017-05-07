6c72234a2268c31f4f9838d42fb3bb3807e1715a
Harish Naik <harishgnaik@gmail.com>
2017-05-07 02:06:02 -0500

    Added logger

---
+++ lib/utils/logger.cpp
@@ -0,0 +1,35 @@
+#include "logger.h"
+
+#include <cstring>
+#include <ctime>
+
+namespace cdi {
+namespace utils {
+
+logging::sources::severity_logger<logging::trivial::severity_level> Logger::log;
+
+/* static */ void Logger::init(const std::string& file_prefix)
+{
+    char buffer[16];
+    memset(buffer, 0, sizeof(buffer));
+    std::time_t time = std::time(nullptr);
+    std::strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S",
+                  std::localtime(&time));
+
+    namespace src = logging::sources;
+    namespace expr = logging::expressions;
+    namespace kw = logging::keywords;
+
+    logging::add_file_log(
+        kw::file_name = file_prefix + "_" + std::string(buffer) + "_%N.log",
+        kw::format = (expr::stream
+                      << "["
+                      << expr::format_date_time<boost::posix_time::ptime>(
+                             "TimeStamp", "%Y%m%d %H:%M:%S")
+                      << "]" << expr::attr<unsigned int>("LineID") << ": <"
+                      << logging::trivial::severity << ">" << expr::smessage));
+
+    logging::add_common_attributes();
+}
+} // namespace utils
+} // namespace cdi
---
+++ lib/utils/logger.h
@@ -0,0 +1,37 @@
+#ifndef CDI_LIB_UTILS_LOGGER_H
+#define CDI_LIB_UTILS_LOGGER_H
+
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/file.hpp>
+
+#define DO_LOG(level) BOOST_LOG_SEV(cdi::utils::Logger::log, level)
+
+#define TRACE() DO_LOG(boost::log::trivial::trace)
+#define DEBUG() DO_LOG(boost::log::trivial::debug)
+#define INFO() DO_LOG(boost::log::trivial::info)
+#define WARN() DO_LOG(boost::log::trivial::info)
+#define ERROR() DO_LOG(boost::log::trivial::info)
+#define FATAL() DO_LOG(boost::log::trivial::info)
+
+namespace cdi {
+namespace utils {
+namespace logging = boost::log;
+class Logger {
+  public:
+    static void init(const std::string& file_prefix = "cdi");
+    static logging::sources::severity_logger<logging::trivial::severity_level>
+        log;
+
+  private:
+};
+
+} // namespace utils
+} // namespace cdi
+
+#endif // CDI_LIB_UTILS_LOGGER_H
