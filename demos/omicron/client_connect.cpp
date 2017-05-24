2eaec87e896428e8167c25738b32ee9decfcf99d
Harish Naik <harishgnaik@gmail.com>
2017-05-23 21:51:57 -0500

    issue-3: enhancement: VICON Demo Code
    This change adds demo code for using VICON mocap infrastructure using the
    Omicron client library. Added BUILD_VICON_CODE and OMICRON_SOURCE_PATH
    variables.

--- demos/omicron/client_connect.cpp
+++ demos/omicron/client_connect.cpp
@@ -1,69 +1,76 @@
 #include <iostream>
 
 #include "connector/omicronConnectorClient.h"
+#include "utils/logger.h"
 
 using namespace omicronConnector;
 
 class Connector_listener : public IOmicronConnectorClientListener {
   public:
+    Connector_listener() {
+        INFO() << "New listener";
+    }
+
     virtual void onEvent(const EventData& event)
     {
         if(EventData::Trace == event.type) {
-            std::cerr << "Trace source: " << event.sourceId << std::endl;
+            INFO() << "Trace source: " << event.sourceId;;
         } else if(EventData::Untrace == event.type) {
-            std::cerr << "Untrade sound: " << event.sourceId << std::endl;
+            INFO() << "Untrace source: " << event.sourceId;
         } else if(EventData::Update == event.type) {
             if(EventData::Update == event.type) {
+                INFO() << "MOCAP source " << event.sourceId << " type ";
                 if(EventData::ServiceTypeMocap == event.serviceType) {
-                    std::cerr << "MOCAP source " << event.sourceId << " type "
-                              << event.type << " device " << event.deviceTag
-                              << std::endl;
+                    INFO() << event.type << " device " << event.deviceTag;
                 }
 
-                std::cout << "pos " << event.posx << "\t" << event.posy << "\t"
-                          << event.posz << std::endl;
-                std::cout << "rot " << event.orx << "\t" << event.ory << "\t"
-                          << event.orz << "\t" << event.orw << std::endl;
-                std::cout << "flag: " << event.flags << std::endl;
-                std::cout << "extra type: " << event.extraDataType << std::endl;
-                std::cout << "extra items: " << event.extraDataItems
-                          << std::endl;
-                std::cout << "extra mask" << event.extraDataMask << std::endl;
+                INFO() << "pos " << event.posx << "\t" << event.posy << "\t"
+                       << event.posz;
+                INFO() << "rot " << event.orx << "\t" << event.ory << "\t"
+                       << event.orz << "\t" << event.orw;
+                INFO() << "flag: " << event.flags;
+                INFO() << "extra type: " << event.extraDataType;
+                INFO() << "extra items: " << event.extraDataItems;
+                INFO() << "extra mask: " << event.extraDataMask;
 
                 if(event.extraDataItems) {
                     if(EventData::ExtraDataFloatArray == event.extraDataType) {
                         float* ptr = (float*)(event.extraData);
                         for(unsigned int k = 0; k < event.extraDataType; ++k) {
-                            std::cout << "val " << k << ptr[k] << std::endl;
+                            INFO() << "val " << k << ptr[k];
                         }
                     } else if(EventData::ExtraDataVector3Array ==
                               event.extraDataType) {
                         float val[3];
                         for(int k = 0; k < event.extraDataItems; ++k) {
                             event.getExtraDataVector3(k, val);
-                            std::cout << "val " << k << ": [" << val[0] << ", "
-                                      << val[1] << ", " << val[2] << std::endl;
+                            INFO() << "val " << k << ": [" << val[0] << ", "
+                                   << val[1] << ", " << val[2];
                         }
                     }
                 }
-                std::cout << "-------------" << std::endl;
+                // INFO() << "-------------" << std::endl;
             }
         } else if(EventData::Down == event.type) {
 
         } else if(EventData::Up == event.type) {
-            std::cout << "EVENT up: " << event.sourceId
-                      << ", type: " << event.type << std::endl;
-            std::cout << "flag: " << event.flags << std::endl;
-            std::cout << "extra: " << event.extraDataType << std::endl;
-            std::cout << "items: " << event.extraDataItems << std::endl;
-            std::cout << "mask: " << event.extraDataMask << std::endl;
-            std::cout << "---------------" << std::endl;
+            INFO() << "EVENT up: " << event.sourceId << ", type: " << event.type
+                   << std::endl;
+            INFO() << "flag: " << event.flags << std::endl;
+            INFO() << "extra: " << event.extraDataType << std::endl;
+            INFO() << "items: " << event.extraDataItems << std::endl;
+            INFO() << "mask: " << event.extraDataMask << std::endl;
+            INFO() << "---------------" << std::endl;
         }
     }
 };
 
-int main()
+int main(int /* argc */, char* argv[])
 {
+    cdi::utils::Logger::init(argv[0]);
+
+    INFO() << "Starting listener";
+
     Connector_listener listener;
     OmicronConnectorClient client(&listener);
     client.connect("131.193.77.108", 28000);
