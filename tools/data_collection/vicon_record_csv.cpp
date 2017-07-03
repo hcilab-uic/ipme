d99e05f6a4afc323df9df428906bcddc12cfad6f
Maxwell Dausch <maxdausch@10-8-227-241.vlan877.client.wireless.uic.edu>
2017-07-03 17:58:16 -0500

    Add Program options to Vicon_record_csv
    Added the ability to specify host address, port,
    output file and the number of points to collect.

--- tools/data_collection/vicon_record_csv.cpp
+++ tools/data_collection/vicon_record_csv.cpp
@@ -1,23 +1,71 @@
 #include <iostream>
 
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
 #include "connector/omicronConnectorClient.h"
 #include "sensor/vicon_csvwriter.h"
 #include "sensor/vicon_listener.h"
 #include "utils/utils.h"
+#include "utils/logger.h"
 
-int main()
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
 {
-    std::string filename{"vicon_record_" +
-                         ipme::utils::create_timestamp_string("%Y%m%d_%H%M%S") +
-                         ".csv"};
+    ipme::utils::Logger::init(argv[0]);
 
-    ipme::sensor::Vicon_listener listener{
-        std::make_unique<ipme::sensor::Vicon_csvwriter>(filename)};
-    omicronConnector::OmicronConnectorClient client(&listener);
+    // clang-format off
+    po::options_description config("Configuration");
+    config.add_options()
+        ("help", "Help Message")
+        ("host,h", po::value<std::string>()->default_value("131.193.77.108"),
+        "Host Address")
+        ("port,p", po::value<short>()->default_value(28000), "Port")
+        ("outputfile,o", po::value<std::string>(), "Output file")
+        ("numpoints,n", po::value<unsigned int>()->required(),
+         "Number of points to collect")
+    ;
+    // clang-format on
 
-    client.connect("131.193.77.108", 28000);
+    try {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, config), vm);
+        po::notify(vm);
 
-    while(listener.event_count() < 5000) {
-        client.poll();
+        if(vm.count("help")) {
+            std::cout << config << "\n";
+            return 0;
+        }
+
+        std::string host = vm["host"].as<std::string>();
+        const short port = vm["port"].as<short>();
+        const unsigned int num_points = vm["numpoints"].as<unsigned int>();
+
+        INFO() << "Connecting to: " << host << ":" << port;
+
+        std::string filename;
+        if(vm.count("outputfile")) {
+            filename = vm["outputfile"].as<std::string>();
+        } else {
+            filename = "vicon_record_" +
+                       ipme::utils::create_timestamp_string("%Y%m%d_%H%M%S") +
+                       ".csv";
+        }
+
+        DEBUG() << "Output file name: " << filename;
+        DEBUG() << "Number of points: " << num_points;
+
+        ipme::sensor::Vicon_listener listener{
+            std::make_unique<ipme::sensor::Vicon_csvwriter>(filename)};
+        omicronConnector::OmicronConnectorClient client(&listener);
+
+        client.connect(host.c_str(), port);
+        while(listener.event_count() < num_points) {
+            client.poll();
+        }
+    } catch(const std::exception& e) {
+        FATAL() << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 }
