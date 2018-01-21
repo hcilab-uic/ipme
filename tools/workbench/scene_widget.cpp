30d6c72e31a26aa4754b986f97ee0f03a607c109
Harish Naik <hnaik2@uic.edu>
2018-01-21 16:01:30 -0600

    Visualization of data inside a circular stencil

--- tools/workbench/scene_widget.cpp
+++ tools/workbench/scene_widget.cpp
@@ -17,15 +17,34 @@ void Scene_widget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_STENCIL_TEST);
+    glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+    glDepthMask(GL_FALSE);
+    glStencilFunc(GL_NEVER, 1, 0xFF);
+    glStencilOp(GL_REPLACE, GL_KEEP, GL_KEEP);
+    glStencilMask(0xFF);
+    glClear(GL_STENCIL_BUFFER_BIT);
 
     // boundary
     Geometry::draw_circle(Point{0.f, 0.f, 0.f}, 1.f, Color{.1f, .1f, .1f, 1.f});
 
+    glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+    glDepthMask(GL_TRUE);
+    glStencilMask(0x00);
+    glStencilFunc(GL_EQUAL, 0, 0xFF);
+    glStencilFunc(GL_EQUAL, 1, 0xFF);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    Geometry::draw_circle(Point{0.f, 0.f, 0.f}, 1.f,
+                          Color{.05f, .05f, .05f, 1.f});
+
     for(const auto& object : objects_) {
         object.draw(show_centerline_, show_tsegment_);
     }
+
+    glDisable(GL_STENCIL_TEST);
 
     glFlush();
 }
