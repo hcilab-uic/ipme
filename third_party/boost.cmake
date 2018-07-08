6543274b6c662ef9287c9b56e1eb6542083553ca
Harish Naik <hnaik2@uic.edu>
2018-07-07 19:11:02 -0500

    Some more boost configuration

--- third_party/boost.cmake
+++ third_party/boost.cmake
@@ -1,20 +1,33 @@
 set(SRC_DIR ../Boost/)
 
+set(boost_rpath "${EXT_INSTALL_DIR}/lib")
+
+set(boost_build
+        ./b2 -j8 install -d+2 --prefix=${EXT_INSTALL_DIR}
+            --layout=tagged
+	    link=shared
+	    runtime-link=shared
+	    dll-path=${boost_rpath}
+	    threading=multi
+)
+
+set(boost_url
+  "https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz")
+
+
 ExternalProject_Add(Boost
   PREFIX                boost
+  # URL                   ${boost_url}
   GIT_REPOSITORY        https://github.com/boostorg/boost.git
   GIT_TAG               boost-1.67.0
-  
-  CMAKE_ARGS            -DBUILD_SHARED_LIBS=ON
-  
+
+  CMAKE_CACHE_ARGS
+      -DBUILD_SHARED_LIBS:BOOL=ON
+
   CONFIGURE_COMMAND    cd ${SRC_DIR} && pwd && ./bootstrap.sh
   BUILD_COMMAND
-      cd ${SRC_DIR} &&
-      pwd &&
-      ./b2 -j8 install --prefix=${EXT_INSTALL_DIR}
+      cd ${SRC_DIR} && pwd && ${boost_build}
+
 
   INSTALL_COMMAND       ""
 )
-
-# ExternalProject_Get_Property(Protobuf EXT_INSTALL_DIR)
-message(STATUS "Install directory ${EXT_INSTALL_DIR}")
