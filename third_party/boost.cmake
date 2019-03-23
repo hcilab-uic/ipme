81d9c18f337dcface46b573fdd5d9289605fabb4
Harish Naik <hnaik2@uic.edu>
2019-03-23 17:38:48 -0500

    Third-party build fix-2
    - Boost b2 command cmake variable name fix
    - OpenCV path fixes

--- third_party/boost.cmake
+++ third_party/boost.cmake
@@ -1,18 +1,30 @@
 set(SRC_DIR ../Boost/)
 
-set(boost_rpath "${EXT_INSTALL_DIR}/lib")
 
+set(prefix_path ${EXT_INSTALL_DIR})
+set(boost_rpath "${prefix_path}/lib")
 set(b2_command "./b2")
 set(bootstrap_command "./bootstrap.sh")
 
+include (ProcessorCount)
+ProcessorCount(N_CORES)
+
+set(url_prefix "https://dl.bintray.com/boostorg/release")
+set(major_version "1")
+set(minor_version "68")
+set(release_build "0")
+set(release_tag "${major_version}.${minor_version}.${release_build}")
+set(file_name "boost_${major_version}_${minor_version}_${release_build}.tar.gz")
+
 if(MSVC)
-	set(b2_command "b2")
-	set(bootstrap_command "bootstrap")
-	message(STATUS "Bootstrapping for Microsoft platform b2 command=${b2_command}, bootstrap command=${bootstrap_command}")
+    set(b2_command "b2")
+    set(bootstrap_command "bootstrap")
+    message(STATUS "Bootstrapping for Microsoft platform b2
+        command=${b2_command}, bootstrap command=${bootstrap_command}")
 endif(MSVC)
 
 set(boost_build
-    ${b2_command} -j8 install -d+2 --prefix=${EXT_INSTALL_DIR}
+    ${b2_command} -j${N_CORES} install -d+2 --prefix=${prefix_path}
         --layout=tagged
         link=shared
         runtime-link=shared
@@ -21,19 +33,18 @@ set(boost_build
         address-model=64
 )
 
-set(boost_url
-  "https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz")
+set(boost_url "${url_prefix}/${release_tag}/source/${file_name}")
 
 ExternalProject_Add(Boost
-  PREFIX                boost
-  URL                   ${boost_url}
+  PREFIX boost
+  URL    ${boost_url}
 
   CMAKE_CACHE_ARGS
       -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
       -DBUILD_SHARED_LIBS:BOOL=ON
-      -DCMAKE_INSTALL_PREFIX:PATH=${EXT_INSTALL_DIR}
-      -DCMAKE_INSTALL_RPATH:PATH=${EXT_INSTALL_DIR}/lib
-      -DCMAKE_PREFIX_PATH:PATH=${EXT_INSTALL_DIR}
+      -DCMAKE_PREFIX_PATH:PATH=${prefix_path}
+      -DCMAKE_INSTALL_PREFIX:PATH=${prefix_path}
+      -DCMAKE_INSTALL_RPATH:PATH=${boost_rpath}
 
   CONFIGURE_COMMAND cd ${SRC_DIR} && pwd && ${bootstrap_command}
 
@@ -41,5 +52,5 @@ ExternalProject_Add(Boost
       cd ${SRC_DIR} && pwd && ${boost_build}
 
   INSTALL_COMMAND       ""
-  INSTALL_DIR           "${EXT_INSTALL_DIR}"
+  INSTALL_DIR           "${prefix_path}"
 )
